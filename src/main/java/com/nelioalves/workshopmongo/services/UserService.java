7ba06e3bf1566b9473796addfc9be0cbcedb8182
Claudio Manoel Goncalves <cmgoncalv70@gmail.com>
2023-06-01 10:37:55 -0300

    Lesson 341 getting an user by ID

--- src/main/java/com/nelioalves/workshopmongo/services/UserService.java
+++ src/main/java/com/nelioalves/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.nelioalves.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -27,8 +28,4 @@ public class UserService {
 		return user;
 	
 	}
-	
-	
-	
-	
 }

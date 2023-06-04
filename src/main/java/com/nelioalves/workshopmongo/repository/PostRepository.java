3da78c70cc168074f9205dc911a610ca41ff339f
Claudio Manoel Goncalves <cmgoncalv70@gmail.com>
2023-06-04 17:21:30 -0300

    Lesson 353 simple survey with @Query

--- src/main/java/com/nelioalves/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/nelioalves/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.nelioalves.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.nelioalves.workshopmongo.domain.Post;
@@ -11,7 +12,11 @@ import com.nelioalves.workshopmongo.domain.Post;
 //parametros: tipo da classe de dominio e tipo do ID da classe
 public interface PostRepository extends MongoRepository<Post, String> {
 	
-	//apenas essa linha já é o query method propriamente dito
-	//List<Post> findByTitleContaining(String text);
+	//?0 significa primeiro parametro que vier no método (nesse caso, text). Se fosse o segundo, seria ?1
+	//A letra i significa ignore case
+	@Query("{ title: { $regex: ?0, $options: 'i' } }")
+	
+	List<Post> searchTitle(String text);	
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);	//opcao para pesquisar maiusculo e minusculo
 }

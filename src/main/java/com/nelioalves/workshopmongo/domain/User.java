4a84ea9a38f455e6197ca0fb913413df7a89c427
Claudio Manoel Goncalves <cmgoncalv70@gmail.com>
2023-06-04 11:23:13 -0300

    Lesson 348 referencing the user posts

--- src/main/java/com/nelioalves/workshopmongo/domain/User.java
+++ src/main/java/com/nelioalves/workshopmongo/domain/User.java
@@ -1,9 +1,13 @@
 package com.nelioalves.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
+
 
 @Document(collection="user")
 public class User implements Serializable {
@@ -14,6 +18,10 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
+	
+	//A lista de posts é uma coleção
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
 	
 	public User() {		
 	}
@@ -49,6 +57,14 @@ public class User implements Serializable {
 		this.email = email;
 	}
 
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}	
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -72,5 +88,5 @@ public class User implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
-	}	
+	}
 }

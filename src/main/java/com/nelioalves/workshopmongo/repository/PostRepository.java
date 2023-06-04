b3dc1f4e5cca480a41b3dae402736dc412c2c6fc
Claudio Manoel Goncalves <cmgoncalv70@gmail.com>
2023-06-04 16:17:41 -0300

    Lesson 352 simple survey with query methods

--- src/main/java/com/nelioalves/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/nelioalves/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.nelioalves.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,8 @@ import com.nelioalves.workshopmongo.domain.Post;
 @Repository
 //parametros: tipo da classe de dominio e tipo do ID da classe
 public interface PostRepository extends MongoRepository<Post, String> {
-
+	
+	//apenas essa linha já é o query method propriamente dito
+	//List<Post> findByTitleContaining(String text);
+	List<Post> findByTitleContainingIgnoreCase(String text);	//opcao para pesquisar maiusculo e minusculo
 }
